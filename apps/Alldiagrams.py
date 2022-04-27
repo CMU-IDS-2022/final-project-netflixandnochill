01b3acdc2c7e3a8cc72aee29e98f133d65376c2c
hams98 <97984418+hams98@users.noreply.github.com>
2022-04-27 18:24:23 -0400

    Add files via upload
    Folder with multiple pages

---
+++ apps/Alldiagrams.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+
+def app():
+
+    st.title('Hello the visualaizations are supposed to be here') 
+    
+    
